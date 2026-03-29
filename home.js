8bd4361f7bd805d65d549f5284ca3b15208febe7
Priya <https:github.com/priyasharma188/my-portfolio.git>
2026-03-29 11:46:54 +0530

    improved style

--- home.js
+++ home.js
@@ -1,25 +1,12 @@
-const textArray = ["I'm a Web Developer", "I'm a Designer", "I'm a Learner"];
-let typingElement = document.getElementById("typing-text");
-let textIndex = 0;
-let charIndex = 0;
 
-function type() {
-  if (charIndex < textArray[textIndex].length) {
-    typingElement.textContent += textArray[textIndex].charAt(charIndex);
-    charIndex++;
-    setTimeout(type, 100);
-  } else {
-    setTimeout(erase, 1500);
-  }
-}
 
-function erase() {
-  if (charIndex > 0) {
-    typingElement.textContent = textArray[textIndex].substring(0, charIndex - 1);
-    charIndex--;
-    setTimeout(erase, 50);
-  } else {
-    textIndex = (textIndex + 1) % textArray.length;
-    setTimeout(type, 500);
-  }
-}
+
+const light = document.querySelector(".light");
+
+document.addEventListener("mousemove",function(e){
+
+light.style.left = e.clientX + "px";
+light.style.top = e.clientY + "px";
+
+});
+
